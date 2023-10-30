c6b0a22eeb55a01d4d6069b895242ff240ad4196
ECSVodacom <matimba.baloyi@vcontractor.co.za>
2023-10-30 09:01:08 +0200

    Update project settings and connection strings to point to production.

--- BizIntegrator.Data/Properties/Settings.Designer.cs
+++ BizIntegrator.Data/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace BizIntegrator.Data.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.101.116,1433;Initial Catalog=BizIntegrator;User ID=sparuser;P" +
-            "assword=ECsqlOnline!;MultipleActiveResultSets=true")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.101.36,1433;Initial Catalog=BizIntegrator;User ID=sparuser;Pa" +
+            "ssword=ECsqlOnline!;MultipleActiveResultSets=true")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
